8e6b7be410703fbd25b99d7f5d12ac5ec6f7d775
Giulia Vieira <giuliadenise10@gmail.com>
2025-02-10 17:17:56 -0300

    alt ALU decoder
    atualizou o decoder, aumentou o ALUControl pra 3bit, e flags de carry e overflow

--- arm_single.sv
+++ arm_single.sv
@@ -229,14 +229,14 @@ module decoder(input  logic [1:0] Op,
   always_comb
     if (ALUOp) begin                 // which DP Instr?
       case(Funct[4:1]) 
-  	    4'b0100: ALUControl = 2'b00; // ADD
-  	    4'b0010: ALUControl = 2'b01; // SUB
-          4'b0000: ALUControl = 2'b10; // AND
-  	    4'b1100: ALUControl = 2'b11; // ORR
+  	    4'b0100: ALUControl = 3'b000; // ADD
+        4'b0010: ALUControl = 3'b001; // SUB
+        4'b0000: ALUControl = 3'b010; // AND
+        4'b1100: ALUControl = 3'b011; // ORR
         4'b0001: ALUControl = 3'b100; // EOR
         4'b1101: ALUControl = 3'b101; // MOV
-        4'b1010: ALUControl = 3'b110; // CMP
-        4'b1000: ALUControl = 3'b111; // TST
+        4'b1010: ALUControl = 3'b110; // CMP (internamente SUB, mas RegWrite=0)
+        4'b1000: ALUControl = 3'b111; // TST (internamente AND, mas RegWrite=0)
   	    default: ALUControl = 2'bx;  // unimplemented
       endcase
       // update flags if S bit is set 
@@ -244,10 +244,12 @@ module decoder(input  logic [1:0] Op,
       FlagW[1]      = Funct[0]; // FlagW[1] = S-bit
 	// FlagW[0] = S-bit & (ADD | SUB)
       FlagW[0]      = Funct[0] & 
-        (ALUControl == 2'b00 | ALUControl == 2'b01); 
+        ((ALUControl == 3'b000) | // ADD
+         (ALUControl == 3'b001) | // SUB
+         (ALUControl == 3'b110)); // CMP
     end else begin
-      ALUControl = 2'b00; // add for non-DP instructions
-      FlagW      = 2'b00; // don't update Flags
+      ALUControl = 3'b000; // add for non-DP instructions
+      FlagW      = 2'b00;  // don't update Flags
     end
               
   // PC Logic
